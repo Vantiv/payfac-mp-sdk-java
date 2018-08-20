d107d4bd6a398b16011d78098d30e39c2e8b271f
Chen Chang <chang.chen2@husky.neu.edu>
2018-08-20 16:12:38 -0400

    add tests for PayFacEception and add some dependencies in build.gradle

--- src/test/java/com/mp/sdk/unitTest/TestCommunication.java
+++ src/test/java/com/mp/sdk/unitTest/TestCommunication.java
@@ -1,4 +1,31 @@
-package com.mp.sdk.unitTest;
-
-public class TestCommunication {
-}
+//package com.mp.sdk.unitTest;
+//
+//import java.util.Properties;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.mp.sdk.Communication;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = Communication.class)
+//public class TestCommunication {
+//    Communication communication;
+//    Properties properties;
+////    Communication communication2;
+//
+//    @Before
+//    public void setUp(){
+//        properties = new Properties();
+//        communication = new Communication(properties);
+////        communication2 = new Communication(new Properties());
+//    }
+//
+//    @Test
+//    public void test(){
+//
+//    }
+//}
--- src/test/java/com/mp/sdk/unitTest/TestPayFacException.java
+++ src/test/java/com/mp/sdk/unitTest/TestPayFacException.java
@@ -1,4 +1,18 @@
 package com.mp.sdk.unitTest;
 
+import org.junit.Test;
+
+import com.mp.sdk.PayFacException;
+
 public class TestPayFacException {
+
+    @Test
+    public void testPayFacException_1(){
+        new PayFacException("test message");
+    }
+
+    @Test
+    public void testPayFacException_2(){
+        new PayFacException("test message",new Exception());
+    }
 }
