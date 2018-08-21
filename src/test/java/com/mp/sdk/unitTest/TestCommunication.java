6812b9537813036952619e6207ae05b1590109c1
cc6980312 <chang.chen2@husky.neu.edu>
2018-08-20 23:55:48 -0400

    delete some useless import in TestCommunication

--- src/test/java/com/mp/sdk/unitTest/TestCommunication.java
+++ src/test/java/com/mp/sdk/unitTest/TestCommunication.java
@@ -2,23 +2,12 @@ package com.mp.sdk.unitTest;
 
 
 import com.mp.sdk.Communication;
-import com.mp.sdk.ErrorResponse;
-import com.mp.sdk.ErrorResponse.Errors;
-import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import java.util.Properties;
-import org.apache.http.HttpResponse;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static com.mp.sdk.Communication.getBestProtocol;
 import static org.junit.Assert.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.nullable;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
 
 public class TestCommunication {
   Communication communication;
